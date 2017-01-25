a1a66f2985622c908bf46a284ffcfdadbe5446d9
hitoshi17L <legend1world@gmail.com>
2017-01-25 17:29:36 +0900

    test4

--- docs/test/assets/js/map.js
+++ docs/test/assets/js/map.js
@@ -80,13 +80,13 @@ function tel_ok(){
 function tw(){
 	p = map.getCenter();
 	zoom = map.getZoom();
-	var url = "http://iamhere.webcrow.jp/index.html#" + zoom + "/" + p.lat + "/" + p.lng;
+	var url = "https://hitoshi17l.github.io/iamherenow/test/index.html%23" + zoom + "/" + p.lat + "/" + p.lng;
 	location.href = "https://twitter.com/share?url=" + url + "&text=%e4%bb%8a%e3%80%81%e3%81%93%e3%81%93%e3%81%ab%e3%81%84%e3%82%8b%e3%82%88%e3%80%82%0d%0a%0d%0a";
 }
 
 function fb(){
 	p = map.getCenter();
 	zoom = map.getZoom();
-	var url = "http://iamhere.webcrow.jp/index.html#" + zoom + "/" + p.lat + "/" + p.lng;
+	var url = "https://hitoshi17l.github.io/iamherenow/test/index.html%23" + zoom + "/" + p.lat + "/" + p.lng;
 	location.href = "https://www.facebook.com/sharer/sharer.php?u=" + url;
 }
