8fc9f6e8691cafc952e403a096c922bcdb05eb31
hitoshi17L <legend1world@gmail.com>
2017-02-18 14:45:52 +0900

    go

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -9,3 +9,23 @@
 
 
 })(jQuery)
+
+function mail(){
+	var url = window.location.href;
+	location.href =  "mailto:?body=MAP URL " + url;
+}
+
+function line(){
+	var url = window.location.href;
+	location.href = "line://msg/text/%e4%bb%8a%e3%80%81%e3%81%93%e3%81%93%e3%81%ab%e3%81%84%e3%82%8b%e3%82%88%e3%80%82%0d%0a%0d%0a"+ url;
+}
+
+function tw(){
+	var url = window.location.href;
+	location.href = "https://twitter.com/share?url=" + url + "&text=%e4%bb%8a%e3%80%81%e3%81%93%e3%81%93%e3%81%ab%e3%81%84%e3%82%8b%e3%82%88%e3%80%82%0d%0a%0d%0a";
+}
+
+function fb(){
+	var url = window.location.href;
+	location.href = "https://www.facebook.com/sharer/sharer.php?u=" + url;
+}
